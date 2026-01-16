0dd7cf810f61db699be7434e951518188bc080e4
leoalfaro868-a11y <leo.alfaro868@students.cmcss.net>
2026-01-16 12:10:08 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,29 @@
+def main():
+    # Enter your color.
+    color1 = "coral"
 
+    print(f"1) Use {color1} to roll a medium-sized ball.\n")
+    # Collect input. Use choice1 Prompt:"big or small? "
+    choice1 = input("big or small? ")
+    # == Is used to test for equality.
+    if choice1 == "big":
+        print("2) Flatten the ball into a wide disc.\n")
+    else:
+        print("2) Keep it as a ball.\n")
+    print("3) Roll four small pieces into short ropes.\n")
+    print("4) Attach them to the bottom.\n")
+    print(f"5) Use {color1} to roll a small ball.\n")
+    print("6) Attach it to one side.\n")
+    #Go to user - Include a space after the ? in your prompt.
+    choice2 = input("option 1 or option 2? ")
+    # Use == to check the User's choice.
+    # Remember you are checking equality to a string. You must use quotes.
+    if choice2 == "option 1":
+        print("7) Pinch the small ball to make it pointy.\n")
+    else:
+        print("7) Keep the small ball round.\n")
+    print("8) Poke two tiny holes in the small ball for eyes.\n")
+    print('9) Write "Turtle" on the name card.')
+
+if __name__ == "__main__":
+    main()
